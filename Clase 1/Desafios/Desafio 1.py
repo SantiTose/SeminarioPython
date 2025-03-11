26d435ce0827d8d04b0207b02f1ec3b21d1cb866
SantiTose <tosettisantino@gmail.com>
2025-03-11 12:21:58 -0300

    Add files via upload

---
+++ Clase 1/Desafios/Desafio 1.py
@@ -0,0 +1,13 @@
+# Desafio 1
+# Queremos ingresar un numero desde teclado e imprimir si el numero es o no par
+numero = int(input('Ingrese un numero: '))
+if numero % 2 == 0:
+    print('Es par ')
+else:
+    print('Es impar')
+
+
+
+
+
+
