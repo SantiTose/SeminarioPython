ad3ba1b77bd8369588493ba0a343b68e3da0a585
SantiTose <tosettisantino@gmail.com>
2025-03-13 11:57:46 -0300

    Fix errors
    Agrego de una comprobacion, para verificar que tipo de respuesta se esta ingresa, es decir si se ingresa una palabra o un digito fuera de rango.

--- Clase 1/Practica 1/questions.py
+++ Clase 1/Practica 1/questions.py
@@ -28,25 +28,47 @@ correct_answers_index = [1, 2, 0, 3, 1]
 2.
 3.
 # El usuario deberá contestar 3 preguntas
+
+exit_status =0
 for _ in range(3):
+    if(exit_status!= 0):
+        break
     # Se selecciona una pregunta aleatoria
     question_index = random.randint(0, len(questions) - 1)
     # Se muestra la pregunta y las respuestas posibles
     print(questions[question_index])
-for i, answer in enumerate(answers[question_index]):
-    print(f"{i + 1}. {answer}")
-    # El usuario tiene 2 intentos para responder correctamente
-for intento in range(2):
-    user_answer = int(input("Respuesta: ")) - 1
-    # Se verifica si la respuesta es correcta
-    if user_answer == correct_answers_index[question_index]:
-        print("¡Correcto!")
-        break
-    else:
-    # Si el usuario no responde correctamente después de 2 intentos,
-    # se muestra la respuesta correcta
-        print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index]
-        [correct_answers_index[question_index]])
-        # Se imprime un blanco al final de la pregunta
-        print()
+    for i, answer in enumerate(answers[question_index]):
+        print(f"{i + 1}. {answer}")
+        # El usuario tiene 2 intentos para responder correctamente
+    for intento in range(2):
+        user_answer = (input("Respuesta: ")) # Verifico si la respuesta solo contiene digitos, de lo contrario termino el bucle
+        if user_answer.isdigit() == True:
+            user_answer= int(user_answer)-1
+            # Se verifica si la respuesta es correcta
+            if user_answer == correct_answers_index[question_index]:
+                print("¡Correcto!")
+                break
+            elif user_answer >=0 and user_answer <=3:
+                if intento == 0:
+                    print('Incorrecto, Te queda 1 intento restante ')
+                    print('Escribe nuevamente tu respuesta ')
+                    continue
+                # Si el usuario no responde correctamente después de 2 intentos,
+                # se muestra la respuesta correcta
+                print("Incorrecto. La respuesta correcta es:")
+                print(answers[question_index]
+                [correct_answers_index[question_index]])
+                # Se imprime un blanco al final de la pregunta
+                print()
+                break
+            else:
+                print('Respuesta invalida! ')
+                exit_status = 1
+                break
+        else:
+            print('Respuesta invalida')
+            exit_status = 1
+            break
+         
+    
+            
