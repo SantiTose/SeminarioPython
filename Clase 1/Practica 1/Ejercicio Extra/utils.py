f54d1af2f95980fa7533312009cae462355687a0
SantiTose <tosettisantino@gmail.com>
2025-03-20 11:28:21 -0300

    Creation of definition agregarProducto

--- Clase 1/Practica 1/Ejercicio Extra/utils.py
+++ Clase 1/Practica 1/Ejercicio Extra/utils.py
@@ -1,1 +1,4 @@
-
+def agregarProducto(inventario = dict):
+    inventario.update({input('Ingrese el nombre del producto: '): input('Ingrese la cantidad de ese producto: ')})
+    print('Elemento agregado correctamente!')
+    print('')
