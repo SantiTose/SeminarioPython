5b15b41007cab7e1c0fcaec7d104368db01f6063
SantiTose <tosettisantino@gmail.com>
2025-03-20 11:28:44 -0300

    creation of defination eliminarProducto

--- Clase 1/Practica 1/Ejercicio Extra/utils.py
+++ Clase 1/Practica 1/Ejercicio Extra/utils.py
@@ -2,3 +2,12 @@ def agregarProducto(inventario = dict):
     inventario.update({input('Ingrese el nombre del producto: '): input('Ingrese la cantidad de ese producto: ')})
     print('Elemento agregado correctamente!')
     print('')
+
+def eliminarProducto(inventario = dict):
+    nom = input('Ingrese el nombre del producto: ')
+    if nom in inventario.keys():
+        inventario.pop(nom)
+        print('Elemento borrado correctamente!!')
+    else:
+        print('No existe esa clave. Ingresa una clave valida! ')
+    print('')
